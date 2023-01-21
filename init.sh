ec92ea844a8120cdb41b92161a1d023c2731682a
edahigure <edahigure@gmail.com>
2023-01-21 00:21:22 -0600

    Remake webpack files

---
+++ init.sh
@@ -0,0 +1,9 @@
+npm init -y
+npm i webpack webpack-cli webpack-dev-server --save-dev
+npm i html-webpack-plugin --save-dev
+npm i webpack-dev-server --save-dev
+npm i css-loader style-loader --save-dev
+npm i css-loader style-loader sass-loader sass --save-dev
+npm i @babel/core babel-loader @babel/preset-env --save-dev
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,31 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: {
-    index: './src/index.js',
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    publicPath: '/',
-  },
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
       },
-    ],
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html")
+    })
+  ],
   optimization: {
-    runtimeChunk: 'single',
+    splitChunks: { chunks: "all" }
   },
-
+  mode: 'development'
 };
