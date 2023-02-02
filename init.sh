c42ede6f3bd6ed7c831298a4d4b03079cde51df4
Edahí Antonio Gutiérrez Reyes <edahigure@gmail.com>
2023-02-01 19:29:53 -0700

    Fix linters

--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
 npm init -y
+npm install --save-dev hint@7.x
+npm install --save-dev stylelint@13.x stylelint-scss@3.x stylelint-config-standard@21.x stylelint-csstree-validator@1.x
+npm install --save-dev eslint@7.x eslint-config-airbnb-base@14.x eslint-plugin-import@2.x babel-eslint@10.x
 npm i webpack webpack-cli webpack-dev-server --save-dev
 npm i html-webpack-plugin --save-dev
 npm i webpack-dev-server --save-dev
